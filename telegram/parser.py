30a83755a8bb769798dc026b497ec54d1c85110d
Koval Yaroslav <yaroslav@koval.page>
2024-05-11 11:06:08 +0200

    Update entity logic

--- telegram/parser.py
+++ telegram/parser.py
@@ -803,36 +803,47 @@ class EntitiesParser:
         )
 
     @staticmethod
-    def length(match: re.Match[str]) -> int:
+    def extract_content(match: re.Match[str], depth: int = 1) -> str:
+        """
+        Extracts the content of a matched entity from within HTML tags.
+
+        Args:
+            match (re.Match[str]): A match object containing the entity.
+            depth (int): Tag immersion depth.
+
+        Returns:
+            str: The extracted content of the matched entity.
+        """
+        return match.group().strip("<>").split(">")[-abs(depth)].split("<")[0]
+
+    def length(self, match: re.Match[str], depth: int = 1) -> int:
         """
         Calculates the length of the matched entity's text content.
 
         Args:
             match (re.Match[str]): A match object containing the entity.
+            depth (int): Tag immersion depth
 
         Returns:
             int: The length of the entity's text content.
         """
-        return len(match.group().strip("<>").split(">")[-1].split("<")[0])
+        return len(self.extract_content(match, depth))
 
-    @staticmethod
-    def offset(text_start: int, match: re.Match[str]) -> int:
+    def offset(self, text_start: int, match: re.Match[str], depth: int = 1) -> int:
         """
         Calculates the end offset of the matched entity in the plain text.
 
         Args:
             text_start (int): The starting index of the entity in the plain text.
             match (re.Match[str]): A match object containing the entity.
+            depth (int): Tag immersion depth
 
         Returns:
             int: The end offset of the entity in the plain text.
         """
-        return text_start + len(
-            match.group().strip("<>").split(">")[-1].split("<")[0]
-        )
+        return text_start + len(self.extract_content(match, depth))
 
-    @staticmethod
-    def text_start(text: str, match: re.Match[str], offset: int) -> int:
+    def text_start(self, text: str, match: re.Match[str], offset: int, depth: int = 1) -> int:
         """
         Finds the starting index of the matched entity's text content in the plain text.
 
@@ -840,12 +851,12 @@ class EntitiesParser:
             text (str): The plain text from which entities are extracted.
             match (re.Match[str]): A match object containing the entity.
             offset (int): The current offset in the plain text to start the search from.
+            depth (int): Tag immersion depth
 
         Returns:
             int: The starting index of the entity in the plain text.
         """
-        return text.find(match.group().strip(
-            "<>").split(">")[-1].split("<")[0], offset)
+        return text.find(self.extract_content(match, depth), offset)
 
     @staticmethod
     def message_type(idx: int) -> str | None:
@@ -884,26 +895,28 @@ class EntitiesParser:
 
         matches = list(re.finditer(self.combined_pattern, self.html_text))
         for match in matches:
-            # Find start position in text only by finding
-            # the index of the next occurrence of the match
-            text_start = self.text_start(self.text_only, match, offset)
-
-            # Update offset to the end of the current match in the text only
-            offset = self.offset(text_start, match)
-
             entity_type = None
             entity_url = None
+            depth = 1
 
             for idx, group in enumerate(match.groups()):
                 if group and idx in self.idx_map:
                     entity_type = self.message_type(idx)
                     if entity_type in ("text_link", "emoji", "animoji",):
                         entity_url = match.group(idx + 2)
+                        depth += 2 if entity_type == "animoji" else 1
                     break
+
+            # Find start position in text only by finding
+            # the index of the next occurrence of the match
+            text_start = self.text_start(self.text_only, match, offset, depth)
+
+            # Update offset to the end of the current match in the text only
+            offset = self.offset(text_start, match, depth)
 
             entity = {
                 "offset": text_start,
-                "length": self.length(match),
+                "length": self.length(match, depth),
                 "type": entity_type
             }
 
