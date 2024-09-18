ff7696cbf9ed4b9a2ae145f8ab78ecdf15d783c1
Yaroslav Koval 🇩🇪 <yaroslav@koval.page>
2024-09-19 00:11:22 +0200

    Add support argument for start.sh
    For example, I you want use 4 workers run "./start.sh 4"

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@
 # Set the default port if not defined
 PORT=${PORT:-8000}
 
-# Calculate the number of workers
-WORKERS=$(( $(nproc) * 2 + 1 ))
+# Check if an argument is passed for workers, otherwise calculate it
+if [ -n "$1" ]; then
+  WORKERS=$1
+else
+  WORKERS=$(( $(nproc) * 2 + 1 ))
+fi
 
 # Run the uvicorn server with the specified settings
-uvicorn app.main:app --workers $WORKERS --host=0.0.0.0 --port="$PORT" --loop uvloop --http h11
+uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --loop uvloop --http h11
