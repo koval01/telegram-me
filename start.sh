ada7f88324d8a64a1b2bb5feebbc8ea2bbf8095e
Koval Yaroslav <yaroslav@koval.page>
2024-10-05 15:32:02 +0200

    Disable access log for production run

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ else
 fi
 
 # Run the uvicorn server with the specified settings
-uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --loop uvloop --http h11
+uvicorn app.main:app --workers "$WORKERS" --host=0.0.0.0 --port="$PORT" --loop uvloop --http h11 --no-access-log
