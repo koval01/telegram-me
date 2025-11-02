d5422fc729447d20a1eacdc717d4f5a0660c3d00
Koval Yaroslav <ksvarog2004@gmail.com>
2025-11-02 17:28:09 +0100

    Add more headers for request to t.me

--- app/telegram/request.py
+++ app/telegram/request.py
@@ -58,10 +58,25 @@ class Request:
                 allow_redirects=False,
                 params=params,
                 headers={
-                    "X-Requested-With": (
+                    "x-requested-with": (
                         "XMLHttpRequest" if method == "POST" else ""
                     ),
-                    "User-Agent": f"TelegramMeAPI/{settings.VERSION} (https://github.com/koval01/telegram-me; yaroslav@koval.page)",  # pylint: disable=line-too-long
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",  # pylint: disable=line-too-long
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "ru",
+                    "cache-control": "max-age=0",
+                    "connection": "keep-alive",
+                    "dnt": "1",
+                    "priority": "u=0, i",
+                    "sec-ch-ua": '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+                    "sec-ch-ua-mobile": "?1",
+                    "sec-ch-ua-platform": "Android",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "none",
+                    "sec-fetch-user": "?1",
+                    "upgrade-insecure-requests": "1",
+                    "user-agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Mobile Safari/537.36",  # pylint: disable=line-too-long
                 },
             ) as response:
                 if response.status != 200:
